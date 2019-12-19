2e0494971783d9151948512b7ad6215d6f7bffd2
Reyber <reybersantos@outlook.es>
2019-12-18 19:40:00 -0600

    upload Exercise #4
    upload Exercise 5
    
    upload Exercise 5

---
+++ spec/controllers/reports_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ReportsController, type: :controller do
+
+  describe "GET #info" do
+    it "Status 200" do
+      get :info
+      expect(response.status).to eq(200)
+    end
+  end
+end
---
+++ spec/routing/reports_routing_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe ReportsController, type: :routing do
+  it "succes route" do
+    expect(:get => "/reports/info").to route_to("reports#info")
+  end
+end
